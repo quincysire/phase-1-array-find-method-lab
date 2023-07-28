5ad4fd8602441ccc79f11b7986bf9f511b7eb343
Yaboisire <Yaboisire2gmail.com>
2023-07-28 10:11:04 +0300

    submit lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+
+ function superbowlWin(array) {
+       if (array.result === "W")
+       {
+            return array["year"][0]
+       } else 
+       {
+          console.log(undefined)
+       }
+   }
+
+console.log(record.find(superbowlWin))
